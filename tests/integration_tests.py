a5d2acc2e191718ad79c41f4096e5cb7611b3a9d
Yanis <yanis.kernoua@epitech.eu>
2023-11-19 12:18:24 +0100

    feat(tests): ✨ execution time

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import shutil
+import time
 
 maps_folder = 'example_files/maps'
 solved_folder = 'example_files/solved'
@@ -16,23 +17,28 @@ os.makedirs(failed_folder)
 for map_file in os.listdir(maps_folder):
     map_path = os.path.join(maps_folder, map_file)
 
+    start = time.time()
     result = subprocess.run(['./setting_up', map_path], capture_output=True, text=True)
+    end = time.time()
+    elapsed_time = round((end - start) * 1000, 5)
 
     solved_file_path = os.path.join(solved_folder, map_file)
     with open(solved_file_path, 'r') as solved_file:
         solved_content = solved_file.read()
 
     if result.stdout == solved_content:
-        print(f"Test passed for {map_file}")
+        print(f"Test passed for {map_file} in {elapsed_time} milliseconds")
         passed_count += 1
         total_count += 1
     else:
-        print(f"Test failed for {map_file}")
+        print(f"Test failed for {map_file} in {elapsed_time} milliseconds")
         failed_count += 1
         total_count += 1
         failed_file_path = os.path.join(failed_folder, map_file)
         with open(failed_file_path, 'w') as failed_file:
             failed_file.write(result.stdout)
+
+    print("--------------------------------------------------")
 
 print(f"Failed : {failed_count}/{total_count}")
 print(f"Passed : {passed_count}/{total_count}")
