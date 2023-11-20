cd291ed5af8234987cb31fda279e7645a4a8ab45
Yanis <yanis.kernoua@epitech.eu>
2023-11-20 20:57:21 +0100

    feat(src/Solver): optimization to solver

--- src/Solver/solver.c
+++ src/Solver/solver.c
@@ -7,27 +7,17 @@
 
 #include <stdlib.h>
 #include "Display/display.h"
+#include "Solver/solver.h"
 #include "include/my_std.h"
 #include "include/my_io.h"
 #include "include/my_strings.h"
-
-static int get_min(int a, int b, int c)
-{
-    int min = a;
-
-    if (b < min)
-        min = b;
-    if (c < min)
-        min = c;
-    return min;
-}
 
 static void check_around(char **board, int **board_clone, int i, int j)
 {
     if (i == 0 || j == 0)
         board_clone[i][j] = board[i][j] == 'o' ? 0 : 1;
     else
-        board_clone[i][j] = board[i][j] == 'o' ? 0 : get_min(
+        board_clone[i][j] = board[i][j] == 'o' ? 0 : GET_MIN(
             board_clone[i - 1][j], board_clone[i][j - 1],
             board_clone[i - 1][j - 1]) + 1;
 }
@@ -42,7 +32,6 @@ int solve_board(char **board, int lines_count, int columns_size)
         board_clone[i] = malloc(sizeof(int) * (columns_size + 1));
         if (board_clone[i] == NULL)
             return EXIT_ERROR;
-        //my_memset(board_clone[i], 0, sizeof(int) * (columns_size + 1));
         for (int j = 0; j < columns_size - 1; j++)
             check_around(board, board_clone, i, j);
         board_clone[i][columns_size] = -1;
